fc5514d84fdb6aa6cb6ca16364b01f65ab72149c
lilclown <djfldrhkdeo97@naver.com>
2022-06-13 09:40:00 +0000

    6/4

--- 항해99/practice/jest-testcode-codeReview/src/index.js
+++ 항해99/practice/jest-testcode-codeReview/src/index.js
@@ -1,22 +1,29 @@
+//this = Site
 class Site {
+    //Site { boards: [] }
     constructor() {
         this.boards = [];
     }
 
+    //boards 안의 board의 name와 추가하는 board의 name이 같을 경우 throw
+    //board.inSite를 true로 바꿈
+    //boards 배열에 board 추가, Board { name: '공지사항', inSite: false, articles: [] }
     addBoard(board) {
-        // console.log(this.boards.includes(board));
         if (this.boards.find((x) => x.name === board.name)) throw Error;
 
         board.inSite = true;
         this.boards.push(board);
     }
 
+    //boards 배열에서 원하는 name을 가진 board 객체 find
     findBoardByName(name) {
         return this.boards.find((x) => x.name === name);
     }
 }
 
+//this = Board
 class Board {
+    //Board { name: '공지사항', inSite: false, articles: [] }
     constructor(name) {
         if (name === null || name === '') throw Error;
 
@@ -25,6 +32,19 @@ class Board {
         this.articles = [];
     }
 
+    //Board의 inSite가 false일 경우 throw
+    //Board에 추가할 때 시간, 랜덤 id 부여
+    //article.inBoard를 true로 바꿈
+    //articles 배열에 article 추가,
+    // Article {
+    //   subject: '글 제목',
+    //   content: '내용',
+    //   author: '작성자',
+    //   inBoard: true,
+    //   comments: [],
+    //   createdDate: '2022-06-04T09:31:14.147Z',
+    //   id: '사이트에 추가된 게시판-0.24156564662695068'
+    // }
     publish(article) {
         if (this.inSite === false) throw Error;
 
@@ -34,14 +54,27 @@ class Board {
         this.articles.push(article);
     }
 
+    //Board안의 articles 전체 조회
     getAllArticles() {
         return this.articles;
     }
 }
 
+//this = Article
 class Article {
+    // Article {
+    //   subject: '글 제목',
+    //   content: '내용',
+    //   author: '작성자',
+    //   inBoard: true,
+    //   comments: [],
+    //   createdDate: '2022-06-04T09:31:14.147Z',
+    //   id: '사이트에 추가된 게시판-0.24156564662695068'
+    // }
+    //subject, contentm, author 중 '', null 인 것이 있을 경우 throw
     constructor(article) {
         const { subject, content, author } = article;
+
         if (
             subject === null ||
             subject === '' ||
@@ -49,9 +82,8 @@ class Article {
             content === '' ||
             author === null ||
             author === ''
-        ) {
-            throw Error();
-        }
+        )
+            throw Error;
 
         this.subject = subject;
         this.content = content;
@@ -60,6 +92,9 @@ class Article {
         this.comments = [];
     }
 
+    //Article의 inBoard가 false일 경우 throw
+    //comment 추가 할 때 시간 부여
+    //Article의 comments 배열에 push, Comment { content: '넵!', author: '댕댕이' }
     reply(comment) {
         if (this.inBoard === false) throw Error;
 
@@ -67,15 +102,18 @@ class Article {
         this.comments.push(comment);
     }
 
+    //Article 안의 comments 전체 조회
     getAllComments() {
         return this.comments;
     }
 }
 
+//this = Comment
 class Comment {
+    //content, author이 null,'' 일 경우 throw
     constructor(comment) {
         const { content, author } = comment;
-        if (content === null || content === '' || author === null || author === '') throw Error();
+        if (content === '' || content === null || author === '' || author === null) throw Error;
 
         this.content = content;
         this.author = author;
