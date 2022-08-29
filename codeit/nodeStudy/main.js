9ab92f2d6e3745147e6ced7d79e1a3d75105c711
lilclown <djfldrhkdeo97@naver.com>
2022-08-29 17:54:44 +0900

    8/29

--- codeit/nodeStudy/main.js
+++ codeit/nodeStudy/main.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req, res) => {
-  console.log(req.url);
-  // res.end('<h1>Hello world!</h1>');
+const app = express();
+
+const users = ['name'];
+
+app.get('/', (req, res) => {
+  res.end('<h1>Welcome!</h1>');
 });
 
-server.listen(3000);
+app.get('/users', (req, res) => {
+  res.end(`<h1>${users}</h1>`);
+});
+
+app.get('/users/:id', (req, res) => {
+  const userName = users[req.params.id - 1];
+  res.end(`<h1>${userName}</h1>`);
+});
+
+app.get('*', (req, res) => {
+  res.end('<h1>Page Not Available</h1>');
+});
+
+app.listen(3000);
